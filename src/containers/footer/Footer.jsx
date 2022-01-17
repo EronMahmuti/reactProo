0996f6b685ba865127fc5d2ac5160fe8e7e430e2
Eron Mahmuti <erooonmahmuti@gmail.com>
2022-01-17 02:55:44 +0100

    push changes

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import './footer.css'
-import gpt3Logo from "../../assets/logo.svg"
+import './footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin , faFacebookSquare , faTwitterSquare , faGithubSquare } from '@fortawesome/free-brands-svg-icons'
 
 export const Footer = () => {
     return (
@@ -10,7 +11,23 @@ export const Footer = () => {
             </div>
 
             <div className="gpt3__footer-btn">
-                <p>I'm active on multiple Social Media platforms. Reach out - let's follow each other and connect.</p>
+                <p>I'm active on multiple Social Media platforms. Reach out - let's follow each other and connect.
+                </p>
+            </div>
+            <div className="gpt3__footer-social">
+                <a href='//www.linkedin.com/in/eron-mahmuti-b06118195/' >
+                    <FontAwesomeIcon className='social linkedin' target='_blank' icon={faLinkedin} />
+                </a>
+                <a href='https://www.facebook.com/eronm' >
+                    <FontAwesomeIcon className='social facebook' icon={faFacebookSquare} />
+                </a>
+                <a href='https://twitter.com/eron_mahmuti' >
+                    <FontAwesomeIcon className='social twitter' icon={faTwitterSquare} />
+                </a>                    
+                <a href='https://github.com/EronGit1' >
+                    <FontAwesomeIcon className='social instagram' icon={faGithubSquare} />
+                </a>
+                
             </div>
 
 
