444466acd9b8fb3a2816904626cdfb6c76eb4b52
Eron Mahmuti <erooonmahmuti@gmail.com>
2022-01-21 01:40:44 +0100

    add some animations

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -9,10 +9,10 @@ export const Possibility = () => {
         }, [])
     return (
         <div className="gpt3__possibility section__padding" id="possibility" >
-                <div className="gpt3__possibility-image" data-aos="fade-right" data-aos-easing="linear" >
+                <div className="gpt3__possibility-image" data-aos="fade-up" data-aos-easing="linear" >
                     <img src={possibilityImage} alt="possibilty" />
                 </div>
-                <div className="gpt3__possibility-content" data-aos="fade-left" data-aos-easing="linear" >
+                <div className="gpt3__possibility-content" data-aos="fade-up" data-aos-easing="linear" >
                     <h4>Request early acces to get started</h4>
                     <h1 className="gradient__text"> Possibilities beyond imagination of the mind and views of the eyes </h1>
                     <p>While I prefer to work with creatives, I will also work with businesses that I feel I would enjoy working with. If you are a restaurant, a barber, e-commerece owner or something of the sort, we can definitely build something cool.</p>
